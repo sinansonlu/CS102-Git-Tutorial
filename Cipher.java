0f2cdb72dc64792e154a0dc21799b0dc4c03e970
Berkin Kağan Ateş <kagan.ates@ug.bilkent.edu.tr>
2022-09-21 12:53:39 +0300

    Bug Fixing
    All bugs have been fixed!

--- Cipher.java
+++ Cipher.java
@@ -12,11 +12,12 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
+        
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
             // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+            outputString += replaceChar(inputString.charAt(i), false);
         }
 
         return outputString;
@@ -28,10 +29,10 @@ public class Cipher
         String outputString = "";
         
         // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
+        for (int i = 0; i < inputString.length(); i++) 
         {
             // append the encrypted version of the char to the output string
-            outputString += replaceChar(inputString.charAt(i), false);
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -42,13 +43,13 @@ public class Cipher
     // if isEncrypt == false -> encrypted to original
     // works only when the input char is included in our alphabet variables
     // should not replace symbols or upper case letters, return input char in those cases
-    private char replaceChar(char inputChar, boolean isEncrypt) {
+    public char replaceChar(char inputChar, boolean isEncrypt) {
         
         if(isEncrypt) {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
