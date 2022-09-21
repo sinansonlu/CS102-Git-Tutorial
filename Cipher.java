ebbdc7fa0dd9900f0e32b3918736e486914ccf1d
Kaanayden <kaanayden15@gmail.com>
2022-09-21 13:54:00 +0300

    Fix encryption and decryption code

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+            outputString += replaceChar(inputString.charAt(i),true);
         }
 
         return outputString;
@@ -27,13 +26,11 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
-        {
-            // append the encrypted version of the char to the output string
-            outputString += replaceChar(inputString.charAt(i), false);
+        for( int i = 0; i < inputString.length(); i++ ) {
+            outputString += replaceChar(inputString.charAt(i),false);
         }
-
+        
+        
         return outputString;
     }
 
@@ -48,7 +45,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
