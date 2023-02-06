39020fe14d7bdddb895309d2b26c0fa34eb3ef19
eececan <ececan@ug.bilkent.edu.tr>
2023-02-06 20:44:18 +0300

    i corrected the for loops and the return statement regarding the cipher alphabet

--- Cipher.java
+++ Cipher.java
@@ -16,7 +16,7 @@ public class Cipher
         for (int i = 0; i < inputString.length(); i++)   
         {
             // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -28,12 +28,11 @@ public class Cipher
         String outputString = "";
         
         // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
+        for (int i = 0; i < inputString.length(); i++) 
         {
             // append the encrypted version of the char to the output string
             outputString += replaceChar(inputString.charAt(i), false);
         }
-ssss
         return outputString;
     }
 
@@ -48,7 +47,7 @@ ssss
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
