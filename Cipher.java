09f8a2892b25ad1d01aef16b4f63d577d1db7ea8
Muhammed Furkan Başıbüyük <114099643+Furkan-Basibuyuk@users.noreply.github.com>
2023-02-08 00:17:08 +0300

    Update Cipher.java
    I think ı fixed the bugs

--- Cipher.java
+++ Cipher.java
@@ -8,26 +8,32 @@ public class Cipher
     public static final String CIPHER_ALPHABET = "dfxyhrklvwuasgimnojpqetbcz";
 
     public String encrypt(String inputString) {
-        
+
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
+
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            // append the encrypted version of the char to the output string
+            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
         }
 
         return outputString;
     }
 
     public String decrypt(String inputString) {
-        
+
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
-        
+
+        // for all chars in the input string
+        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
+        {
+            // append the encrypted version of the char to the output string
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
+
         return outputString;
     }
 
@@ -37,12 +43,12 @@ public class Cipher
     // works only when the input char is included in our alphabet variables
     // should not replace symbols or upper case letters, return input char in those cases
     private char replaceChar(char inputChar, boolean isEncrypt) {
-        
+
         if(isEncrypt) {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    ohpqog CIPHER_ALPHABET.xldoAp(v);
                 }
             }
         }
@@ -54,7 +60,7 @@ public class Cipher
                 }
             }
         }
-        
+
         // if we did not find it in the alphabet, then return the original char
         return inputChar;
     }
