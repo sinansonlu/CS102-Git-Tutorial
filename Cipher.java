8290aa220fab126ee46f48671d5ad95acb8b2652
Cankutay Dündar <cankutay.dundar@icloud.com>
2023-02-08 10:49:14 +0300

    I fixed problems
    I fixed problems

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -26,14 +25,12 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
-        {
-            // append the encrypted version of the char to the output string
+        for(int i = 0 ; i < inputString.length() ; i++){
+
             outputString += replaceChar(inputString.charAt(i), false);
         }
-
+        replaceChar('a',true);
+        
         return outputString;
     }
 
@@ -48,7 +45,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
