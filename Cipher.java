de721c7122ea590a51215e36f54c51ecb7d45e71
Egehan <egehanyldz7@gmail.com>
2023-02-08 09:07:14 +0300

    Son değişiklikler yapıldı

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            outputString += replaceChar(inputString.charAt(i), false);
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -26,7 +26,7 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         for(int i = 0; i<inputString.length();i++){
-            outputString += replaceChar(inputString.charAt(i),true);
+            outputString += replaceChar(inputString.charAt(i), false);
         }
 
         return outputString;
