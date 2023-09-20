587dc64401dab130217c09ec033450673291b16c
Atakan Akar <126462032+AtakanAkar04@users.noreply.github.com>
2023-09-20 16:03:23 +0300

    Update Cipher.java
    I changed the errors successfully. There were some errors in for loops in the methods which encrypt and decrypt. And there was a return type error in replace char method.

--- Cipher.java
+++ Cipher.java
@@ -43,16 +43,16 @@ public class Cipher
         if(isEncrypt) {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
-                if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                        return CIPHER_ALPHABET.charAt(i);
+                if(CIPHER_ALPHABET.charAt(i) == inputChar) {
+                        return ORIGINAL_ALPHABET.charAt(i);
                 }
             }
         }
         else {
             for (int i = 0; i < CIPHER_ALPHABET.length(); i++)   
             {
-                if(CIPHER_ALPHABET.charAt(i) == inputChar) {
-                    return ORIGINAL_ALPHABET.charAt(i);
+                if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
