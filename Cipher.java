ee9760a84a803e8190636e01bd9130dd26f4ca72
YusufGulluce <yag2703@gmail.com>
2023-09-22 11:10:38 +0300

    Chiper_fixed

--- Cipher.java
+++ Cipher.java
@@ -12,10 +12,11 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
+
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -26,7 +27,11 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        replaceChar('a',true);
+        // for all chars in the input string
+        for (int i = 0; i < inputString.length(); i++)   
+        {
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
         
         return outputString;
     }
@@ -37,12 +42,14 @@ public class Cipher
     // works only when the input char is included in our alphabet variables
     // should not replace symbols or upper case letters, return input char in those cases
     private char replaceChar(char inputChar, boolean isEncrypt) {
+
         
+
         if(isEncrypt) {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
