51419618110fd87ad4d12a8d0e4536c59d8f2d02
Serdar Kara <serdark4ra@gmail.com>
2024-02-04 14:56:52 +0300

    Error Fixed
    A small mistake in the code fixed

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
@@ -25,6 +25,10 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
+
+        for (int i = 0; i < inputString.length(); i++) {
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
         
         replaceChar('a',true);
         
@@ -42,7 +46,8 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    //corrected
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
