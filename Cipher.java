d398a2d4cb5e7cc20d7d61231bf5ca49bfae863f
Emre3555 <emrealtuner8@gmail.com>
2024-02-07 10:36:22 +0300

    Changes in Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+            outputString += replaceChar(inputString.charAt(i),true);
         }
 
         return outputString;
@@ -26,14 +25,11 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
+        for(int i = 0;i<inputString.length();i++)
         {
-            // append the encrypted version of the char to the output string
-            outputString += replaceChar(inputString.charAt(i), false);
+            outputString += replaceChar(inputString.charAt(i),false);
         }
-
+        
         return outputString;
     }
 
@@ -44,24 +40,30 @@ public class Cipher
     // should not replace symbols or upper case letters, return input char in those cases
     private char replaceChar(char inputChar, boolean isEncrypt) {
         
-        if(isEncrypt) {
+        if(isEncrypt) 
+        {
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+                    inputChar = CIPHER_ALPHABET.charAt(i);
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
+            return inputChar;
         }
-        else {
-            for (int i = 0; i < CIPHER_ALPHABET.length(); i++)   
+        else 
+        { 
+        
+           for (int i = 0; i < CIPHER_ALPHABET.length(); i++)   
             {
                 if(CIPHER_ALPHABET.charAt(i) == inputChar) {
+                    inputChar = ORIGINAL_ALPHABET.charAt(i);
                     return ORIGINAL_ALPHABET.charAt(i);
                 }
             }
-        }
         
         // if we did not find it in the alphabet, then return the original char
         return inputChar;
-    }
-}   
+        }
+    }   
+}
