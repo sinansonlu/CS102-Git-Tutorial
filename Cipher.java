af77d0194a2a01b66f815507ded6d3d84a479993
BerraAlkis <146773314+BerraAlkis@users.noreply.github.com>
2024-02-08 16:20:29 +0300

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -11,11 +11,13 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
+        char ch = ' ';
         
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            ch = inputString.chart(İ);
+            replaceChar(ch,false);
         }
 
         return outputString;
@@ -25,8 +27,14 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
+        char ch = ' ';
         
-        replaceChar('a',true);
+        for (int i = 0; i < inputString.length(); i++)   
+        {
+            ch = inputString.chart(İ);
+            replaceChar(ch, true);
+        }
+
         
         return outputString;
     }
@@ -42,7 +50,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
